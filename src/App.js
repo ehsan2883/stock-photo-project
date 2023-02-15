624e09cab0b317f72f16eec96b0a1394064a08bc
ehsan2883 <ehsanmansouri979@gmail.com>
2023-02-15 14:07:35 +0330

    Fetch Data From Unsplash

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+const clientId = `?client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}`;
+const url = `https://api.unsplash.com/photos/${clientId}`;
 
 const App = () => {
-  return (
-    <div>Hi!</div>
-  )
-}
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
 
-export default App
+  const fetchData = async (url) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error.response);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(url);
+  }, []);
+
+  return <div>Hi!</div>;
+};
+
+export default App;
